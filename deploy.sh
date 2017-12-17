058335624b1c50ff649ec41386dfeb0f36420fb9
Andrey Shemyakov <andrey.shemiakov@gmail.com>
2017-12-17 14:30:30 +0300

    Deploy to Heroku

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-set -e
-zip -r build.zip build
-
-curl -H "Content-type: application/zip" \
-     -H "Authorization: Bearer $NETLIFY_KEY" \
-     --data-binary "@build.zip" \
-     https://api.netlify.com/api/v1/sites
